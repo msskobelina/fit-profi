35b30df7ead1e7e4626a27c776df0cc71dedbcdf
Maryna Skobelina <maryna.skobelina@tentens.tech>
2026-01-18 21:52:33 +0200

    feat: fix backup

--- scripts/backup/entrypoint.sh
+++ scripts/backup/entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
-echo "[ENTRYPOINT] Starting backup cron..."
-
-cat >/etc/cron.d/fitprofi-mysql-backup <<'CRON'
+cat <<EOF > /etc/crontab
 SHELL=/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 0 3 * * * root /scripts/full_backup.sh >> /var/log/full_backup.log 2>&1
-0 * * * * root /scripts/inc_backup.sh  >> /var/log/inc_backup.log 2>&1
-CRON
+0 * * * * root /scripts/inc_backup.sh >> /var/log/inc_backup.log 2>&1
+EOF
 
-chmod 0644 /etc/cron.d/fitprofi-mysql-backup
 touch /var/log/full_backup.log /var/log/inc_backup.log
 
-cron
-tail -F /var/log/full_backup.log /var/log/inc_backup.log
+echo "[ENTRYPOINT] Starting crond..."
+crond -n
