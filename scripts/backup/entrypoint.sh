9d016e52c8af1538a9c52b041d766f377ee5caca
Maryna Skobelina <maryna.skobelina@tentens.tech>
2026-01-28 00:12:41 +0200

    feat: fix cron

--- scripts/backup/entrypoint.sh
+++ scripts/backup/entrypoint.sh
@@ -1,19 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-echo "[ENTRYPOINT] Setting up cron jobs..."
+cat <<EOF > /etc/crontab
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-mkdir -p /etc/crontabs
+0 3 * * * root /scripts/full_backup.sh >> /var/log/full_backup.log 2>&1
+0 * * * * root /scripts/inc_backup.sh >> /var/log/inc_backup.log 2>&1
 
-cat > /etc/crontabs/root <<'EOF'
-0 3 * * * /scripts/full_backup.sh >> /var/log/full_backup.log 2>&1
-0 * * * * /scripts/inc_backup.sh >> /var/log/inc_backup.log 2>&1
 EOF
 
-chmod 600 /etc/crontabs/root
-
-echo "[ENTRYPOINT] Cron jobs installed:"
-cat /etc/crontabs/root
+touch /var/log/full_backup.log /var/log/inc_backup.log
 
 echo "[ENTRYPOINT] Starting crond..."
-exec crond -n -l 2
+crond -n
